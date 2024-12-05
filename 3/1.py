1f958a99c613954d277a6957d71248f3e921af38
Gregor <43413852+Albadylic@users.noreply.github.com>
2024-12-05 18:40:54 +0000

    Add some comments

--- 3/1.py
+++ 3/1.py
@@ -5,17 +5,23 @@ multiplyregex = 'mul\(\d{1,3},\d{1,3}\)'
 doregex = 'do\(\)'
 dontregex = "don\'t\(\)"
 
-
+# Split the original string on every time that 'do()' occurs
 domatches = re.split(doregex, input)
 
+# Split the list by don't()'s
 def splitthematches(el):
     return re.split(dontregex, el)
-
 splitbydont = list(map(splitthematches, domatches))
+
+# Delete every second item, to ignore the cases with 'dont'
 del splitbydont[1::2]
+
+# Join the lists into a string
 def joinlist(arr):
     return ''.join(arr)
 newinput = ''.join(list(map(joinlist, splitbydont)))
+
+# Do what we did in part 1
 matches = re.findall(multiplyregex, newinput)
 
 def removefluff(el):
@@ -30,3 +36,4 @@ result = sum(list(map(multiply, map(removefluff, matches))))
 print(result)
 
 # 79233424 is too low
+# 81852502 is too low
